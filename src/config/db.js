e7d4cf964a6a71145f3d2954de80cfc5874b1b4d
shaile.global-at-077818350833 <shaile.martin@gmail.com>
2025-08-16 22:39:06 +0530

    env

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,3 @@
-// db.js (Firestore version)
 const { Firestore } = require("@google-cloud/firestore");
-
-// Initialize Firestore client
 const db = new Firestore();
-
-console.log("✅ Firestore connected successfully");
-
 module.exports = db;
